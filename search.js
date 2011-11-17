690be697cf702c8a86b99023fc8757f20a20e646
hemant mistry <hemant.mistry@wazoku.com>
2011-11-17 14:43:29 +0000

    updated

--- search.js
+++ search.js
@@ -1,55 +1,20 @@
-(function 	($) {
-  jQuery.expr[':'].Contains = function(a,i,m){
-      return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
-  };
-
-
-  function listFilter(header, list) { 
-    var form = $("<form>").attr({"class":"filterform","action":"#"}),
-        input = $("<input>").attr({"class":"filterinput","type":"text"});
-    $(form).append(input).appendTo(header);
-
-    $(input)
-      .change( function () {
-        var filter = $(this).val();
-        if(filter) {
-
-          $(list).find("a:not(:Contains(" + filter + "))").parent().slideUp();
-          $(list).find("a:Contains(" + filter + ")").parent().slideDown();
-        } else {
-          $(list).find("li").slideDown();
-        }
-        return false;
-      })
-    .keyup( function () {
-
-        $(this).change();
+$(document).ready(function(){
+    $("#filter").keyup(function(){
+ 
+        var filter = $(this).val(), count = 0;
+ 
+        $(".profilelist li").each(function(){
+ 
+            if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+                $(this).fadeOut();
+ 
+            } else {
+                $(this).show();
+                count++;
+            }
+        });
+ 
+        var numberItems = count;
+        $("#filter-count").text("Number of Comments = "+count);
     });
-  }
-
-
-  $(function () {
-    listFilter($("#header"), $("#list"));
-  });
-}(jQuery));
-
-$(document).ready(function () {
-	$('#search').keyup(function(event) {
-		var search_text = $('#search').val();
-		var rg = new RegExp(search_text,'i');
-		$('#product_list .product-list .product').each(function(){
- 			if($.trim($(this).html()).search(rg) == -1) {
-				$(this).parent().css('display', 'none');
- 				$(this).css('display', 'none');
-				$(this).next().css('display', 'none');
-				$(this).next().next().css('display', 'none');
-			}	
-			else {
-				$(this).parent().css('display', '');
-				$(this).css('display', '');
-				$(this).next().css('display', '');
-				$(this).next().next().css('display', '');
-			}
-		});
-	});
 });
