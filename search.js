bd59feb48af3f86d0fcb0db238718563ed1f664c
hemant mistry <hemant.mistry@wazoku.com>
2011-11-22 15:14:22 +0000

    adding tabs, changing css

--- search.js
+++ search.js
@@ -23,3 +23,33 @@ $(document).ready(function(){
 			return false;
 		});
 });
+
+
+
+
+			<!--tabs with idea-->
+			
+								<div id="container">
+								  <div id="tabs">
+								    <ul>
+								      <li><a href="#tab-1">Tab 1</a></li>
+								      <li><a href="#tab-2">Tab Two</a></li>
+								      <li><a href="#tab-3">Tab Three</a></li>
+								    </ul>
+								    <div id="tab-1">
+								      <h3>Ideas</h3>
+								      <p><img src=""/> <br/>
+								        <a href=""></a> </p>
+								    </div>
+								    <div id="tab-2">
+								      <h3>Ideas</h3>
+								      <p><img src=""/> <br/>
+								        <a href=""></a></p>
+								    </div>
+								    <div id="tab-3">
+								      <h3>Ideas</h3>
+								      <p><img src=""/><br/>
+								        <a href=""></a></p>
+								    </div>
+								  </div>
+								</div>
